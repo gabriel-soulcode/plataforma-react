e1c9152fac0ef557a5693b25ffeb10f253c0685b
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-21 15:17:41 -0300

    Menu com React Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import { Menu } from "./components/Menu/Menu";
 import { Home } from './pages/Home/Home';
 import { Blog } from './pages/Blog/Blog';
 import { Contato } from './pages/Contato/Contato';
+import { Usuarios } from './pages/Usuarios/Usuarios';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contato" element={<Contato />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/usuarios" element={<Usuarios />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
