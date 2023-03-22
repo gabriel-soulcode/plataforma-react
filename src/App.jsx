41ae0ec70de8eba284325c2d2114ad2981900404
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-22 11:14:43 -0300

    Componente Root

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Menu } from "./components/Menu/Menu";
 import { Home } from './pages/Home/Home';
 import { Blog } from './pages/Blog/Blog';
 import { Contato } from './pages/Contato/Contato';
 import { Login } from './pages/Login/Login';
 import { Usuarios } from './pages/Usuarios/Usuarios';
+import { NotFound } from './pages/NotFound/NotFound';
+import { Root } from './pages/Root/Root';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Menu />
-          <Routes>
+      <BrowserRouter>
+        <Routes>
+          {/* 
+            ANINHAMENTO DE ROTAS
+            exemplo.com/usuarios/perfil
+          */}
+          <Route path="/" element={<Root />}>
             <Route path="/" element={<Home />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/contato" element={<Contato />} />
             <Route path="/usuarios" element={<Usuarios />} />
             <Route path="/login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Usuarios/Usuarios.jsx
+++ src/pages/Usuarios/Usuarios.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export function Usuarios() {
     return (
         <div className="usuarios">
             <h1>Usuarios</h1>
+            <Outlet />
         </div>
     )
 }
