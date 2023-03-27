0d8e39ba7dc35cf6393883e2a0c64987d17837cb
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-26 21:12:16 -0300

    Desafio de Formulario

--- src/pages/Programadores/Programadores.jsx
+++ src/pages/Programadores/Programadores.jsx
@@ -1,11 +1,22 @@
-import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Alert, Button, Form, Modal } from "react-bootstrap";
 import { useForm } from "react-hook-form"
 
 export function Programadores() {
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [data, setData] = useState({});
+
+    const patternEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    function onSubmit(data) {
+    function onSubmit(dev) {
+        setData(dev);
+        setShow(true);
     }
 
     return (
@@ -14,28 +25,28 @@ export function Programadores() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group className="mb-3">
                     <Form.Label>Nome completo</Form.Label>
-                    <Form.Control {...register("nome")} />
-                    {errors.nome && <span className="invalid">Digite um nome válido!</span>}
+                    <Form.Control {...register("nome", { required: true, maxLength: 255 })} />
+                    {errors.nome && <Alert variant="danger" className="py-1 mt-2">Nome inválido!</Alert>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>E-mail</Form.Label>
-                    <Form.Control {...register("email")} />
-                    {errors.email && <span className="invalid">Digite um email válido!</span>}
+                    <Form.Control {...register("email", { required: true, maxLength: 255, pattern: patternEmail })} />
+                    {errors.email && <Alert variant="danger" className="py-1 mt-2">E-mail inválido!</Alert>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Telefone</Form.Label>
-                    <Form.Control {...register("telefone")} />
-                    {errors.telefone && <span className="invalid">Digite um telefone válido!</span>}
+                    <Form.Control {...register("telefone", { required: true, maxLength: 255 })} />
+                    {errors.telefone && <Alert variant="danger" className="py-1 mt-2">Telefone inválido!</Alert>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Estado</Form.Label>
-                    <Form.Control {...register("estado")} />
-                    {errors.estado && <span className="invalid">Digite um estado válido!</span>}
+                    <Form.Control {...register("estado", { required: true, maxLength: 255 })} />
+                    {errors.estado && <Alert variant="danger" className="py-1 mt-2">Estado inválido!</Alert>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Cidade</Form.Label>
-                    <Form.Control {...register("cidade")} />
-                    {errors.cidade && <span className="invalid">Digite um cidade válido!</span>}
+                    <Form.Control {...register("cidade", { required: true, maxLength: 255 })} />
+                    {errors.cidade && <Alert variant="danger" className="py-1 mt-2">Cidade inválida!</Alert>}
                 </Form.Group>
                 <Form.Label>Interesses</Form.Label>
                 <Form.Group className="mb-3" controlId="back">
@@ -49,18 +60,19 @@ export function Programadores() {
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Nível de experiência</Form.Label>
-                    <Form.Select {...register("nivel")}>
+                    <Form.Select {...register("nivel", { required: true, maxLength: 255 })}>
                         <option>Júnior</option>
                         <option>Pleno</option>
                         <option>Sênior</option>
                     </Form.Select>
+                    {errors.nivel && <Alert variant="danger" className="py-1 mt-2">Nível de experiência inválido!</Alert>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Portfólio</Form.Label>
-                    <Form.Control {...register("portfolio")} />
-                    {errors.portfolio && <span className="invalid">Digite um portfolio válido!</span>}
+                    <Form.Control {...register("portfolio", { required: true, maxLength: 255 })} />
+                    {errors.portfolio && <Alert variant="danger" className="py-1 mt-2">Endereço do portfólio inválido!</Alert>}
                 </Form.Group>
-                    <Form.Label>Habilidades</Form.Label>
+                <Form.Label>Habilidades</Form.Label>
                 <Form.Group className="mb-3" controlId="html">
                     <Form.Check type="checkbox" label="HTML" {...register("html")} />
                 </Form.Group>
@@ -72,11 +84,50 @@ export function Programadores() {
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Formação acadêmica</Form.Label>
-                    <Form.Control {...register("academico")} />
-                    {errors.academico && <span className="invalid">Digite uma formação academica válida!</span>}
+                    <Form.Control {...register("academico", { required: true, maxLength: 255 })} />
+                    {errors.academico && <Alert variant="danger" className="py-1 mt-2">Formação acadêmica inválida!</Alert>}
                 </Form.Group>
-                <Button variant="dark">Enviar</Button>
+                <Button variant="dark" type="submit">Enviar</Button>
             </form>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Informações do Dev</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ul>
+                        <li>{data.nome}</li>
+                        <li>{data.email}</li>
+                        <li>{data.telefone}</li>
+                        <li>{data.estado}</li>
+                        <li>{data.cidade}</li>
+                        <li>{data.nivel}</li>
+                        <li>{data.academico}</li>
+                        <li>{data.portfolio}</li>
+                        <li>
+                            Habilidades
+                            <ul>
+                                <li>{data.js ? "Possuo" : "Não possuo"} habilidade em JavaScript</li>
+                                <li>{data.html ? "Possuo" : "Não possuo"} habilidade em HTML</li>
+                                <li>{data.css ? "Possuo" : "Não possuo"} habilidade em CSS</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Interesses
+                            <ul>
+                                <li>{data.back ? "Possuo" : "Não possuo"} interesse em Back-End</li>
+                                <li>{data.front ? "Possuo" : "Não possuo"} interesse em Front-End</li>
+                                <li>{data.mobile ? "Possuo" : "Não possuo"} interesse em Mobile</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
